5389d3f22d9d17090ac242f25ef97766e29088ef
andrew-bubbi <141924810+andrew-bubbi@users.noreply.github.com>
2024-03-05 19:29:36 +0200

    chore: add domain to origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://shop-one-sage.vercel.app"],
   });
   await app.listen(3000);
 }
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { ProductsService } from "./products.service";
 import { ProductsController } from "./products.controller";
-import { KeyGuard } from "./guards/products.guard";
 
 @Module({
   controllers: [ProductsController],
